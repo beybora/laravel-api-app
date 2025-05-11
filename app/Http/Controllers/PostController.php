b02d242607a019defda92c6820c88f8cd8c4ed05
beybora <bora.b.sarikaya@gmail.com>
2025-05-11 21:49:51 +0200

    add middleware and policy checks for post update and delete endpoints

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware as ControllersMiddleware;
+use Illuminate\Support\Facades\Gate;
 
-class PostController extends Controller
+class PostController extends Controller implements HasMiddleware
 {
+    public static function middleware()
+    {
+        return [
+            new ControllersMiddleware('auth:sanctum', except: ['index', 'show'])
+        ];
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -27,7 +38,7 @@ class PostController extends Controller
             ]
         );
 
-        $post =  Post::create($fields);
+        $post =  $request->user()->posts()->create($fields);
 
         return $post;
     }
@@ -45,6 +56,8 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+        Gate::authorize('modify', $post);
+
         $fields = $request->validate(
             [
                 'title' => 'required',
@@ -62,6 +75,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        Gate::authorize('modify', $post);
+
         $post->delete();
 
         return ['message' => 'The post was deleted'];
